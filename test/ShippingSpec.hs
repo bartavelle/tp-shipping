7044565667627d0fa046e19dfc52446050394030
Simon Marechal <simon.marechal@synacktiv.com>
2020-10-14 14:35:47 +0200

    test updates

--- test/ShippingSpec.hs
+++ test/ShippingSpec.hs
@@ -3,25 +3,32 @@ module ShippingSpec (shippingSpec) where
 
 import qualified Data.Map.Strict        as M
 import           Test.Hspec
-import           Test.Hspec.QuickCheck  (prop)
+import           Test.Hspec.QuickCheck  ( prop )
 import           Test.QuickCheck
 
+import           Control.Monad          ( replicateM )
+import           Data.List              ( nub )
 import           Shipping
-import           ShippingBot.Definition (Complication (..))
-import           ShippingBot.Prop       (propBot)
+import           ShippingBot.Definition ( Complication (..) )
+import           ShippingBot.Prop       ( propBot )
 
 genStocks :: Gen (Stock, Stock)
 genStocks = (,) <$> genStock <*> genStock
 
 genStock :: Gen Stock
 genStock = do
-  keys <- suchThat (sublistOf [0..100]) (not . null)
-  M.fromList <$> mapM (\k -> (ItemId k,) <$> elements[1..100] ) keys
+  stocksize <- elements [1..10]
+  keys <- nub <$> replicateM stocksize (elements [1..20])
+  M.fromList <$> mapM (\k -> (ItemId k,) <$> elements [1..100] ) keys
 
 shippingSpec :: Spec
 shippingSpec = do
   let qstock = M.findWithDefault 0
       defDest = Destination "destination" Standard
+  it "increaseStock case 1" $
+    increaseStock (M.singleton 1 1) (M.singleton 1 2) `shouldBe` M.singleton 1 3
+  it "increaseStock case 2" $
+    increaseStock (M.singleton 1 1) (M.singleton 2 2) `shouldBe` M.fromList [(1,1),(2,2)]
   describe "increaseStock" $ do
     prop "additivity" $ forAll genStocks $ \(s1, s2) ->
       let s' = increaseStock s1 s2
@@ -32,11 +39,19 @@ shippingSpec = do
             s2' = M.delete k s2
             s'  = increaseStock s1' s2'
         in  qstock k s' == 0
-  prop "decreaseStock" $ forAll genStocks $ \(s1, s2) ->
+  it "decreaseStock case 1" $
+    decreaseStock (M.singleton 1 3) (M.singleton 1 2) `shouldBe` Just (M.singleton 1 1)
+  it "decreaseStock case 2" $
+    decreaseStock (M.singleton 1 3) (M.singleton 1 3) `shouldBe` Just M.empty
+  it "decreaseStock case 3" $
+    decreaseStock (M.singleton 1 3) (M.singleton 2 2) `shouldBe` Nothing
+  it "decreaseStock case 4" $
+    decreaseStock (M.singleton 1 3) (M.singleton 1 4) `shouldBe` Nothing
+  prop "decreaseStock prop" $ forAll genStocks $ \(s1, s2) ->
     let diff = M.unionWith (+) s1 (fmap negate s2)
     in case decreaseStock s1 s2 of
       Nothing -> any (< 0) diff
-      Just r  -> all (> 0) r && r == M.filter (/= 0) diff
+      Just r -> all (> 0) r && r == M.filter (/= 0) diff
   describe "initialShippingState" $
     prop "is today" (\n -> let d = Day n in getDay (initialShippingState d) == d)
   let istate = initialShippingState 0
@@ -47,15 +62,15 @@ shippingSpec = do
       trackingA = TrackingId 1233145
 
   describe "order" $ do
-    it "will work when stock is available" $ do
+    describe "when stock is available" $ do
       let (stock', _, events) = order order0 (OrderInformation (M.singleton 1 5) defDest) istock istate
-      stock' `shouldBe` M.fromList [(1, 5), (2, 5)]
-      events `shouldBe` Just (ShippingInformation order0 defDest)
-    it "will wait when stock is not available" $ do
+      it "decreases stock" $ stock' `shouldBe` M.fromList [(1, 5), (2, 5)]
+      it "sent the proper shipping info" $ events `shouldBe` Just (ShippingInformation order0 defDest)
+    describe "when stock is not available" $ do
       let (stock', stt', events) = order order0 (OrderInformation (M.singleton 2 10) defDest) istock istate
-      stock' `shouldBe` istock
-      events `shouldBe` Nothing
-      getWaitingOrders stt' `shouldBe` [order0]
+      it "does not alter stock" $ stock' `shouldBe` istock
+      it "does not send shipping information" $ events `shouldBe` Nothing
+      it "lists a waiting order" $ getWaitingOrders stt' `shouldBe` [order0]
     describe "initialShippingState order" $ do
       it "has no waiting orders" $ getWaitingOrders istate `shouldBe` []
       it "has no orders waiting for a tracking number" $ getWaitingTracking istate `shouldBe` []
